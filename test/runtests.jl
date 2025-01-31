6dabbdc0e658e3f24ab7dc55f092a8195e70414b
jeffersonfparil <jeffersonparil@gmail.com>
2025-01-31 17:14:07 +1100

    initialising the main package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 using GBCore
-=======
-using GenomicBreeding
->>>>>>> 7b2d10d3735c6d1b76158a74a72edb4cdd8f5b60
 using Test
 using Documenter
 
 Documenter.doctest(GBCore)
 
-<<<<<<< HEAD
 @testset "GBCore.jl" begin
-=======
-@testset "GenomicBreeding.jl" begin
->>>>>>> 7b2d10d3735c6d1b76158a74a72edb4cdd8f5b60
     @test 1 == 1
 end
