abb9b1b5a950f3a77c0bd46857ed7fa424318b8a
jeffersonfparil <jeffersonparil@gmail.com>
2025-11-21 13:36:23 +1100

    adding Documenter setup in test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 using GenomicBreeding
 using Test
-using Documenter
+using Documenter, DocumenterTools
 
-Documenter.doctest(GenomicBreeding)
+try
+    Documenter.doctest(GenomicBreeding)
+catch
+    DocumenterTools.generate()
+    Pkg.activate("docs/")
+    Documenter.doctest(GenomicBreeding)
+end
 
 @testset "GenomicBreeding.jl" begin
     @test 1 == 1
